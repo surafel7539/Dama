a6fbb295b1d6e2bd6bd504ca85aba4cbf2b07ba2
surafel7539 <suchaz7539@gmail.com>
2026-07-31 21:27:51 +0300

    changed Home to home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 // Pages
-import Home from './pages/Home';
+import Home from './pages/home';
 import Marketplace from './pages/Marketplace';
 import ProductDetails from './pages/ProductDetails';
 import Categories from './pages/Categories';
