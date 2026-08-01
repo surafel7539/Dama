e54b18f013f4d668a2f088841303249d5a24c4b7
surafel7539 <suchaz7539@gmail.com>
2026-08-01 16:05:54 +0300

    Fixed some more bugs

--- Backend/server.js
+++ Backend/server.js
@@ -21,18 +21,10 @@ const PORT = process.env.PORT || 5000;
 // Core Middlewares
 // Replace the allowedOrigins array and app.use(cors(...)) with this:
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://dama-ach4-alpha.vercel.app' 
-];
-
 app.use(cors({
-  origin: allowedOrigins,
+  origin: "https://dama-ach4-alpha.vercel.app",
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 
 
 app.use(express.json());
